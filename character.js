b6489671cf1677a19f0b5396de9ddfd67fb8c835
Mikle <miklenomeny@gmail.com>
2025-02-04 19:25:01 +0300

    AI minimax implementation, still bugs

--- character.js
+++ character.js
@@ -1,47 +1,67 @@
 let user1 = document.getElementById("user1");
 let user2 = document.getElementById("user2");
 let user3 = document.getElementById("user3");
+let user4 = document.getElementById("user4");
+let user5 = document.getElementById("user5");
+let user6 = document.getElementById("user6");
+let user7 = document.getElementById("user7");
+let user8 = document.getElementById("user8");
+let user9 = document.getElementById("user9");
+let user10 = document.getElementById("user10");
+let user11 = document.getElementById("user11");
+let user12 = document.getElementById("user12");
+let user13 = document.getElementById("user13");
+let user14 = document.getElementById("user14");
+let user15 = document.getElementById("user15");
+let user16 = document.getElementById("user16");
 
 let btnContinue = document.getElementById("btn-continue");
 let character = document.getElementById("character");
 let current_char = user1;
 let playerProfile = document.getElementById("player-profile");
 
-let users = [user1, user2, user3];
+let users = [user1, user2, user3, user4, user5, user6, user7, user8, user9, user10, user11, user12, user13, user14, user15, user16];
 
-user1.addEventListener("click", function(){
-    current_char = user1;
-    playerProfile.src = current_char.src;
-    adjustScaleImg(this);
-})
+for (i = 0; i < users.length; i++) {
+    let c = users[i];
 
-user2.addEventListener("click", function(){
-    current_char = user2;
-    playerProfile.src = current_char.src;
-    adjustScaleImg(this);
-})
+    c.addEventListener("click", function(){
+        current_char = c;
+        playerProfile.src = current_char.src;
+        adjustScaleImg(c);
+    })
 
-user3.addEventListener("click", function(){
-    current_char = user3;
-    playerProfile.src = current_char.src;
-    adjustScaleImg(this);
-})
+}
 
-btnContinue.addEventListener("click", function(){
-    if(current_char != null)
-    {
+// user1.addEventListener("click", function(){
+//     current_char = user1;
+//     playerProfile.src = current_char.src;
+//     adjustScaleImg(this);
+// })
+
+// user2.addEventListener("click", function(){
+//     current_char = user2;
+//     playerProfile.src = current_char.src;
+//     adjustScaleImg(this);
+// })
+
+// user3.addEventListener("click", function(){
+//     current_char = user3;
+//     playerProfile.src = current_char.src;
+//     adjustScaleImg(this);
+// })
+
+btnContinue.addEventListener("click", function () {
+    if (current_char != null) {
         character.style.display = "none";
         playerProfile.src = current_char.src
     }
 })
 
-function adjustScaleImg(user)
-{
-    user.style.transform = "scale(1.2)"
-    for(i=0; i<users.length; i++)
-    {
-        if(users[i] != user)
-        {
+function adjustScaleImg(user) {
+    user.style.transform = "scale(1.1)"
+    for (i = 0; i < users.length; i++) {
+        if (users[i] != user) {
             users[i].style.transform = "scale(1.0)"
         }
     }
--- logic.js
+++ logic.js
@@ -1,6 +1,8 @@
 let player = "X";
 let opponent = "O";
 
+let gameStart = false;
+
 let playerCanMark = true;
 let opponentCanMark = false;
 let isGameOver = false;
@@ -36,162 +38,302 @@ let firstCol = [case1, case4, case7];
 let secondCol = [case2, case5, case8];
 let thirdCol = [case3, case6, case9];
 
+//stocking each oblique
+let firstObl = [case1, case5, case9];
+let secondObl = [case3, case5, case7];
+
 let cases = [case1, case2, case3, case4, case5, case6, case7, case8, case9];
 
-for(i=0; i<cases.length; i++)
-{
-    cases[i].addEventListener("click", function(){
-        placeMark(this);
-    })
+
+for (i = 0; i < cases.length; i++) {
+    if (playerCanMark) {
+        cases[i].addEventListener("click", function () {
+            placeMark(this);
+        })
+    }
 }
 
-function placeMark(x)
-{
-    if(!isGameOver) //not game over
+function placeMark(x) {
+    if (!isGameOver) //not game over
     {
         moveLeftCheck() //check if there are move left
         //player place his mark
-        if(x.innerText == "." && playerCanMark)
-        {
+        if (x.innerText == ".") {
             x.innerText = player; // X
             playerCanMark = false;
             opponentCanMark = true;
             playerProfile.style.transform = "scale(1.0)";
             opponentProfile.style.transform = "scale(1.5)";
             emptyCase -= 1;
+            setTimeout(() => {
+                bestMoveAi();
+            }, 2000)
         }//opponent place his mark
-        else if(x.innerText == "." && opponentCanMark)
-        {
-            x.innerText = opponent; // O
-            playerCanMark = true;
-            opponentCanMark = false;
-            playerProfile.style.transform = "scale(1.5)";
-            opponentProfile.style.transform = "scale(1.0)";
-            emptyCase -= 1;
-        }
     }
     checkPlayerWin();
-    checkOppenentWin();
 }
 
-function gameOver()
-{
+function gameOver() {
     emptyCase = 0;
     isGameOver = true;
 
-    if(hasPlayerWon)
-    {
-        alert("Player One Won");
-    }else if(hasOpponentWon)
-    {
-        alert("Player Two Won");
+    if (hasPlayerWon) {
+        // alert("Player One Won");
+    } else if (hasOpponentWon) {
+        // alert("Player Two Won");
     }
 }
 
-function moveLeftCheck()
-{
-    if(emptyCase <= 0)
-    {
+function moveLeftCheck() {
+    if (emptyCase <= 0) {
         //disable click
         alert("No move left")
     }
 }
 
-function checkPlayerWin()
-{
+function checkPlayerWin() {
     //horizontal check
-    if(case1.innerText == player && case2.innerText == player && case3.innerText == player)
-    {
+    if (case1.innerText == player && case2.innerText == player && case3.innerText == player) {
+        for (i = 0; i < firstRow.length; i++) {
+            firstRow[i].style.transition = "0.5s ease";
+            firstRow[i].style.color = "white";
+            firstRow[i].style.background = "green";
+        }
         hasPlayerWon = true;
-        gameOver();
+        // gameOver();
     }
-    if(case4.innerText == player && case5.innerText == player && case6.innerText == player)
-    {
+    if (case4.innerText == player && case5.innerText == player && case6.innerText == player) {
+        for (i = 0; i < secondRow.length; i++) {
+            secondRow[i].style.transition = "0.5s ease";
+            secondRow[i].style.color = "white";
+            secondRow[i].style.background = "green";
+        }
         hasPlayerWon = true;
-        gameOver();
+        // gameOver();
     }
-    if(case7.innerText == player && case8.innerText == player && case9.innerText == player)
-    {
+    if (case7.innerText == player && case8.innerText == player && case9.innerText == player) {
+        for (i = 0; i < thirdRow.length; i++) {
+            thirdRow[i].style.transition = "0.5s ease";
+            thirdRow[i].style.color = "white";
+            thirdRow[i].style.background = "green";
+        }
         hasPlayerWon = true;
-        gameOver();
+        // gameOver();
     }
 
     //vertical check
-    if(case1.innerText == player && case4.innerText == player && case7.innerText == player)
-    {
+    if (case1.innerText == player && case4.innerText == player && case7.innerText == player) {
+        for (i = 0; i < firstCol.length; i++) {
+            firstCol[i].style.transition = "0.5s ease";
+            firstCol[i].style.color = "white";
+            firstCol[i].style.background = "green";
+        }
         hasPlayerWon = true;
-        gameOver();
+        // gameOver();
     }
-    if(case2.innerText == player && case5.innerText == player && case8.innerText == player)
-    {
+    if (case2.innerText == player && case5.innerText == player && case8.innerText == player) {
+        for (i = 0; i < secondCol.length; i++) {
+            secondCol[i].style.transition = "0.5s ease";
+            secondCol[i].style.color = "white";
+            secondCol[i].style.background = "green";
+        }
         hasPlayerWon = true;
-        gameOver();
+        // gameOver();
     }
-    if(case3.innerText == player && case6.innerText == player && case9.innerText == player)
-    {
+    if (case3.innerText == player && case6.innerText == player && case9.innerText == player) {
+        for (i = 0; i < thirdCol.length; i++) {
+            thirdCol[i].style.transition = "0.5s ease";
+            thirdCol[i].style.color = "white";
+            thirdCol[i].style.background = "green";
+        }
         hasPlayerWon = true;
-        gameOver();
+        // gameOver();
     }
 
     //oblique check
-    if(case1.innerText == player && case5.innerText == player && case9.innerText == player)
-    {
+    if (case1.innerText == player && case5.innerText == player && case9.innerText == player) {
+        for (i = 0; i < firstObl.length; i++) {
+            firstObl[i].style.transition = "0.5s ease";
+            firstObl[i].style.color = "white";
+            firstObl[i].style.background = "green";
+        }
         hasPlayerWon = true;
-        gameOver();
+        // gameOver();
     }
-    if(case3.innerText == player && case5.innerText == player && case7.innerText == player)
-    {
+    if (case3.innerText == player && case5.innerText == player && case7.innerText == player) {
+        for (i = 0; i < secondObl.length; i++) {
+            secondObl[i].style.transition = "0.5s ease";
+            secondObl[i].style.color = "white";
+            secondObl[i].style.background = "green";
+        }
         hasPlayerWon = true;
-        gameOver();
+        // gameOver();
     }
 }
 
-function checkOppenentWin()
-{
+function checkOppenentWin() {
     //horizontal check
-    if(case1.innerText == opponent && case2.innerText == opponent && case3.innerText == opponent)
-    {
+    if (case1.innerText == opponent && case2.innerText == opponent && case3.innerText == opponent) {
+        for (i = 0; i < firstRow.length; i++) {
+            firstRow[i].style.transition = "0.5s ease";
+            firstRow[i].style.color = "white";
+            firstRow[i].style.background = "red";
+        }
         hasOpponentWon = true;
-        gameOver();
+        // gameOver();
     }
-    if(case4.innerText == opponent && case5.innerText == opponent && case6.innerText == opponent)
-    {
+    if (case4.innerText == opponent && case5.innerText == opponent && case6.innerText == opponent) {
+        for (i = 0; i < secondRow.length; i++) {
+            secondRow[i].style.transition = "0.5s ease";
+            secondRow[i].style.color = "white";
+            secondRow[i].style.background = "red";
+        }
         hasOpponentWon = true;
-        gameOver();
+        // gameOver();
     }
-    if(case7.innerText == opponent && case8.innerText == opponent && case9.innerText == opponent)
-    {
+    if (case7.innerText == opponent && case8.innerText == opponent && case9.innerText == opponent) {
+        for (i = 0; i < thirdRow.length; i++) {
+            thirdRow[i].style.transition = "0.5s ease";
+            thirdRow[i].style.color = "white";
+            thirdRow[i].style.background = "red";
+        }
         hasOpponentWon = true;
-        gameOver();
+        // gameOver();
     }
 
     //vertical check
-    if(case1.innerText == opponent && case4.innerText == opponent && case7.innerText == opponent)
-    {
+    if (case1.innerText == opponent && case4.innerText == opponent && case7.innerText == opponent) {
+        for (i = 0; i < firstCol.length; i++) {
+            firstCol[i].style.transition = "0.5s ease";
+            firstCol[i].style.color = "white";
+            firstCol[i].style.background = "red";
+        }
         hasOpponentWon = true;
-        gameOver();
+        // gameOver();
     }
-    if(case2.innerText == opponent && case5.innerText == opponent && case8.innerText == opponent)
-    {
+    if (case2.innerText == opponent && case5.innerText == opponent && case8.innerText == opponent) {
+        for (i = 0; i < secondCol.length; i++) {
+            secondCol[i].style.transition = "0.5s ease";
+            secondCol[i].style.color = "white";
+            secondCol[i].style.background = "green";
+        }
         hasOpponentWon = true;
-        gameOver();
+        // gameOver();
     }
-    if(case3.innerText == opponent && case6.innerText == player && case9.innerText == player)
-    {
+    if (case3.innerText == opponent && case6.innerText == player && case9.innerText == player) {
+        for (i = 0; i < thirdCol.length; i++) {
+            thirdCol[i].style.transition = "0.5s ease";
+            thirdCol[i].style.color = "white";
+            thirdCol[i].style.background = "green";
+        }
         hasOpponentWon = true;
-        gameOver();
+        // gameOver();
     }
 
     //oblique check
-    if(case1.innerText == opponent && case5.innerText == opponent && case9.innerText == opponent)
-    {
+    if (case1.innerText == opponent && case5.innerText == opponent && case9.innerText == opponent) {
+        for (i = 0; i < firstObl.length; i++) {
+            firstObl[i].style.transition = "0.5s ease";
+            firstObl[i].style.color = "white";
+            firstObl[i].style.background = "green";
+        }
         hasOpponentWon = true;
-        gameOver();
+        // gameOver();
     }
-    if(case3.innerText == opponent && case5.innerText == opponent && case7.innerText == opponent)
-    {
+    if (case3.innerText == opponent && case5.innerText == opponent && case7.innerText == opponent) {
+        for (i = 0; i < secondObl.length; i++) {
+            secondObl[i].style.transition = "0.5s ease";
+            secondObl[i].style.color = "white";
+            secondObl[i].style.background = "green";
+        }
         hasOpponentWon = true;
-        gameOver();
+        // gameOver();
     }
 }
 
+let scores = {
+    'X': 1,
+    'O': -1,
+    'tie': 0
+}
+
+function checkWinner() {
+    checkPlayerWin()
+    checkOppenentWin()
+    if (hasPlayerWon) {
+        return 'X';
+    } else if (hasOpponentWon) {
+        return 'O';
+    } else if (!hasPlayerWon && !hasOpponentWon && emptyCase <= 0) {
+        return "tie";
+    } else {
+        return null;
+    }
+}
+
+function minimax(cases, depth, isMaximizing) {
+    let result = checkWinner();
+    console.log(result)
+    if (result !== null) {
+        return scores[result];
+    }
+
+    if (isMaximizing) {
+        let bestScore = -Infinity;
+        for (let i = 0; i < cases.length; i++) {
+            //check if the case is empty
+            if (cases[i].innerText == ".") {
+                cases[i].innerText = opponent;
+                let score = minimax(cases, depth + 1, false);
+                cases[i].innerText = ".";
+                bestScore = Math.max(score, bestScore);
+            }
+        }
+        return bestScore;
+
+    } else {
+        let bestScore = Infinity;
+        for (let i = 0; i < cases.length; i++) {
+            //check if the case is empty
+            if (cases[i].innerText == ".") {
+                cases[i].innerText = player;
+                let score = minimax(cases, depth + 1, true);
+                cases[i].innerText = ".";
+                bestScore = Math.min(score, bestScore);
+            }
+        }
+
+        return bestScore;
+    }
+}
+
+function bestMoveAi() {
+    //AI 's bestMove
+    let bestScore = -Infinity;
+    let move;
+
+    for (let i = 0; i < cases.length; i++) {
+        //check if the case is empty
+        if (cases[i].innerText == ".") {
+            cases[i].innerText = opponent;
+            let score = minimax(cases, 0, false);
+            console.log(score)
+            cases[i].innerText = ".";
+
+            if (score > bestScore) {
+                bestScore = score;
+                move = i;
+                console.log(i)
+            }
+        }
+    }
+
+    cases[move].innerText = opponent;
+    playerCanMark = true;
+    opponentCanMark = false;
+    playerProfile.style.transform = "scale(1.5)";
+    opponentProfile.style.transform = "scale(1.0)";
+    emptyCase -= 1;
+    checkOppenentWin();
+
+}
